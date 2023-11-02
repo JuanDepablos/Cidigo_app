5e5163fb60a44768596e9387de732336e4dcdbb5
JuanDepablos <juandepablos.s@gmail.com>
2023-11-02 14:41:22 -0500

    repo de la pagina

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'tu_clave_secreta'  
-
+app.secret_key = 'tu_clave_secreta'
 
 def connect_to_database():
     try:
@@ -12,80 +11,65 @@ def connect_to_database():
             database='proyecto',
             user='root',
             password=None,
-            port=3307 
+            port=3307
         )
         return conn
     except Exception as e:
         print(e)
         return None
 
-# Ruta para procesar el inicio de sesión (POST)
-@app.route('/', methods=['POST'])
-def login_post():
-    username = request.form['username']
-    password = request.form['password']
-
-    conn = connect_to_database()
-    if conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM usuarios WHERE username = %s AND password = %s", (username, password))
-        user = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        if user:
-            session['user_id'] = user[0]
-            flash('Inicio de sesión exitoso', 'success')
-            return redirect(url_for('profile'))
-        else:
-            flash('Perfil no encontrado. Verifica tus credenciales.', 'danger')
-
-    return redirect(url_for('login'))
-
-# Ruta para procesar el registro de usuario (POST)
-@app.route('/registro', methods=['POST'])
-def registro_post():
-    nombre = request.form['nombre']
-    username = request.form['username']
-    correo = request.form['correo']
-    password = request.form['password']
-    confirm_password = request.form['confirm_password']
-
-    print(request.form)  # Imprime los datos del formulario para depuración
-    nombre = request.form['nombre']
-    
-    if password != confirm_password:
-        flash('Las contraseñas no coinciden', 'danger')
-        return redirect(url_for('registro'))
-
-    conn = connect_to_database()
-    if conn:
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO usuarios (nombre, username, correo, password) VALUES (%s, %s, %s, %s)",
-                       (nombre, username, correo, password))
-        conn.commit()
-        cursor.close()
-        conn.close()
-
-        flash('Registro exitoso. Ahora puedes iniciar sesión.', 'success')
-        return redirect(url_for('login'))
-    else:
-        flash('Error en la base de datos. Inténtalo de nuevo.', 'danger')
-
-    return redirect(url_for('registro'))
-
-# Ruta para la página de inicio (GET)
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM usuarios WHERE username = %s AND password = %s", (username, password))
+            user = cursor.fetchone()
+            cursor.close()
+            conn.close()
+
+            if user:
+                session['user_id'] = user[0]
+                flash('Inicio de sesión exitoso', 'success')
+                return redirect(url_for('profile'))
+            else:
+                flash('Perfil no encontrado. Verifica tus credenciales.', 'danger')
+
     return render_template('login.html')
 
-# Ruta para la página de registro (GET)
-@app.route('/registro', methods=['GET'])
+@app.route('/registro', methods=['POST', 'GET'])
 def registro():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        username = request.form['username']
+        correo = request.form['correo']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            flash('Las contraseñas no coinciden', 'danger')
+        else:
+            conn = connect_to_database()
+            if conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO usuarios (nombre, username, correo, password) VALUES (%s, %s, %s, %s)",
+                               (nombre, username, correo, password))
+                conn.commit()
+                cursor.close()
+                conn.close()
+
+                flash('Registro exitoso. Ahora puedes iniciar sesión.', 'success')
+                return redirect(url_for('login'))
+            else:
+                flash('Error en la base de datos. Inténtalo de nuevo.', 'danger')
+
     return render_template('registro.html')
 
-# Ruta para el perfil del usuario (GET)
-@app.route('/profile', methods=['GET'])
+@app.route('/profile')
 def profile():
     if 'user_id' in session:
         user_id = session['user_id']
@@ -98,15 +82,74 @@ def profile():
             conn.close()
 
             if user:
-                return render_template('inicio.html', user=user)
-    
+                nombre_usuario = user[2]
+                return render_template('inicio.html', nombre=nombre_usuario)
+
     flash('Inicia sesión para ver tu perfil', 'info')
     return redirect(url_for('login'))
-@app.route('/info', methods=['GET'])
+
+@app.route('/info')
 def info():
     # Lógica para mostrar la información de envío
     return render_template('info.html')
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor(buffered=True)
+            cursor.execute("SELECT * FROM usuarios WHERE id = %s", (user_id,))
+            user = cursor.fetchone()
+            cursor.close()
+
+            if user:
+                if request.method == 'POST':
+                    new_nombre_usuario = request.form['nombre_usuario']
+                    new_correo = request.form['correo']
+                    new_password = request.form['password']
+
+                    # Validar que el nuevo correo no esté en uso
+                    if new_correo != user[3]:  # Evita la validación si el usuario mantiene su correo actual
+                        cursor = conn.cursor(buffered=True)
+                        cursor.execute("SELECT id FROM usuarios WHERE correo = %s", (new_correo,))
+                        existing_user = cursor.fetchone()
+                        cursor.close()
+                        if existing_user:
+                            flash('El correo ya está en uso. Elije otro.', 'danger')
+                            return redirect(url_for('edit_profile'))
+
+                    # Validar que el nuevo nombre de usuario no esté en uso
+                    if new_nombre_usuario != user[2]:
+                        cursor = conn.cursor(buffered=True)
+                        cursor.execute("SELECT id FROM usuarios WHERE username = %s", (new_nombre_usuario,))
+                        existing_user = cursor.fetchone()
+                        cursor.close()
+                        if existing_user:
+                            flash('El nombre de usuario ya está en uso. Elije otro.', 'danger')
+                            return redirect(url_for('edit_profile'))
+
+                    # Actualizar la información del usuario en la base de datos
+                    cursor = conn.cursor(buffered=True)
+                    cursor.execute("UPDATE usuarios SET username = %s, correo = %s WHERE id = %s", (new_nombre_usuario, new_correo, user_id))
+                    conn.commit()
+                    cursor.close()
+
+                    # Actualizar la contraseña si se proporcionó una nueva
+                    if new_password:
+                        cursor = conn.cursor(buffered=True)
+                        cursor.execute("UPDATE usuarios SET password = %s WHERE id = %s", (new_password, user_id))
+                        conn.commit()
+                        cursor.close()
+
+                    flash('Cambios guardados exitosamente', 'success')
+                    return redirect(url_for('profile'))
+
+                return render_template('editar_info.html', usuario=user)  # Nombre del archivo HTML
+
+    flash('Inicia sesión para acceder a esta página', 'info')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
